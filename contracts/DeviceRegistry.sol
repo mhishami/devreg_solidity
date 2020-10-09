8d2d613518c48dd599e9a8a1fe8c61c0805d4eb0
Hisham Ismail <mhishami@gmail.com>
2020-10-09 17:37:30 +0800

    Added view, rem device

--- contracts/DeviceRegistry.sol
+++ contracts/DeviceRegistry.sol
@@ -2,15 +2,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-contract DeviceRegistry {
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+contract DeviceRegistry is Ownable {
 
   mapping(address => mapping(bytes32 => bool)) private deviceList;
 
-  function addDevice(address user, string memory deviceId) public returns (bool) {
+  event deviceAdded(address indexed user, bytes32 indexed deviceHash);
+  event deviceRemoved(address indexed user, bytes32 indexed deviceHash);
+
+  function addDevice(address user, string memory deviceId) public onlyOwner returns (bool) {
     bytes32 deviceHash = keccak256(abi.encodePacked(deviceId));
     deviceList[user][deviceHash] = true;
+
+    emit deviceAdded(user, deviceHash);
 
     return true;
   }
   
+  function removeDevice(address user, string memory deviceId) public onlyOwner returns (bool) {
+    bytes32 deviceHash = keccak256(abi.encodePacked(deviceId));
+    deviceList[user][deviceHash] = false;
+
+    emit deviceRemoved(user, deviceHash);
+
+    return true;
+  }
+
+  function validate(string memory deviceId) public view returns (bool) {
+    bytes32 deviceHash = keccak256(abi.encodePacked(deviceId));
+    return deviceList[msg.sender][deviceHash];
+  }
 }

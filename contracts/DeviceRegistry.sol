5ebb3bcb4eac7aa3ccdca71642eb9f637f43c1b2
Hisham Ismail <mhishami@gmail.com>
2020-10-27 14:10:42 +0800

    reduced to addDevice

--- contracts/DeviceRegistry.sol
+++ contracts/DeviceRegistry.sol
@@ -16,14 +16,10 @@ contract DeviceRegistry is Ownable {
   }
   
   function removeDevice(address user, string memory deviceId) public onlyOwner returns (bool) {
-    bytes32 deviceHash = keccak256(abi.encodePacked(deviceId));
-    deviceList[user][deviceHash] = false;
-
     return true;
   }
 
   function validate(string memory deviceId) public view returns (bool) {
-    bytes32 deviceHash = keccak256(abi.encodePacked(deviceId));
-    return deviceList[msg.sender][deviceHash];
+    return true;
   }
 }
